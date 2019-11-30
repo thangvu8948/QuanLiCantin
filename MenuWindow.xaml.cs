dfbcd75484159aa1e4fcb8978d447902c2f7f9e5
Vũ Phạm Đức Thắng <1712763@student.hcmus.edu.vn>
2019-11-30 22:49:10 +0700

    + lọc danh sách thức ăn theo loại

--- MenuWindow.xaml.cs
+++ MenuWindow.xaml.cs
@@ -46,7 +46,7 @@ namespace QuanLiCantin
                 Debug.WriteLine("Openning Connection ...");
                 conn.Open();
                 Debug.WriteLine("Connection successful!");
-                BindingList<Product> products = ProductDAO.GetAllProducts(conn);
+                BindingList<Product> products = ProductDAO.GetAllProducts(_typeFood);
                 if (products != null)
                 {
                     MessageBox.Show($"{products[0].Name} \r\n {products[0].Price}");
@@ -86,76 +86,87 @@ namespace QuanLiCantin
 
         BindingList<Product> _products = null;
         BindingList<Order> _orders = null;
+        int _typeFood = 0;
 
-        class ProductDAO
+        class ProductDAO : INotifyPropertyChanged
         {
-            public static BindingList<Product> GetAllProducts(SqlConnection conn)
+            public event PropertyChangedEventHandler PropertyChanged;
+
+            public static BindingList<Product> GetAllProducts( int type)
             {
                 string sql = "Select * from MonAn";
-
-                // Tạo một đối tượng Command.
-                SqlCommand cmd = new SqlCommand();
-
-                // Liên hợp Command với Connection.
-                cmd.Connection = conn;
-                cmd.CommandText = sql;
-
-
-                using (DbDataReader reader = cmd.ExecuteReader())
+                SqlConnection conn = DBUtils.GetDBConnection();
+                try
                 {
-                    if (reader.HasRows)
+                    conn.Open();
+                    switch (type)
                     {
-                        BindingList<Product> listProduct = new BindingList<Product>();
-                        while (reader.Read())
-                        {
-                            // Chỉ số của cột Emp_ID trong câu lệnh SQL.
-                            string id = Convert.ToString(reader.GetValue(0)).Trim(); // 0
-                            string tenmon = Convert.ToString(reader.GetValue(1));
-                            long giatien = Convert.ToInt64(reader.GetValue(2));
-                            int soluong = Convert.ToInt32(reader.GetValue(3));
-                            int loai = Convert.ToInt32(reader.GetValue(4));
-
-
-                            var product = new Product()
-                            {
-                                ID = id,
-                                Name = tenmon,
-                                Price = giatien,
-                                Remain = soluong,
-                                Type = loai,
-                                Image = $"Images/{id}/download.jpg"
-                            };
-                            listProduct.Add(product);
-                        }
-                        return listProduct;
+                        case 0: sql = "Select * from MonAn"; break;
+                        case 1: sql = "Select * from MonAn where MALOAI = 1"; break;
+                        case 2: sql = "Select * from MonAn where MALOAI = 2"; break;
+                        case 3: sql = "Select * from MonAn where MALOAI = 3"; break;
                     }
-                    return null;
+
+                    // Tạo một đối tượng Command.
+                    SqlCommand cmd = new SqlCommand();
+
+                    // Liên hợp Command với Connection.
+                    cmd.Connection = conn;
+                    cmd.CommandText = sql;
+
+
+                    using (DbDataReader reader = cmd.ExecuteReader())
+                    {
+                        if (reader.HasRows)
+                        {
+                            BindingList<Product> listProduct = new BindingList<Product>();
+                            while (reader.Read())
+                            {
+                                // Chỉ số của cột Emp_ID trong câu lệnh SQL.
+                                string id = Convert.ToString(reader.GetValue(0)).Trim(); // 0
+                                string tenmon = Convert.ToString(reader.GetValue(1));
+                                long giatien = Convert.ToInt64(reader.GetValue(2));
+                                int soluong = Convert.ToInt32(reader.GetValue(3));
+                                int loai = Convert.ToInt32(reader.GetValue(4));
+
+
+                                var product = new Product()
+                                {
+                                    ID = id,
+                                    Name = tenmon,
+                                    Price = giatien,
+                                    Remain = soluong,
+                                    Type = loai,
+                                    Image = $"Images/{id}/download.jpg"
+                                };
+                                listProduct.Add(product);
+                            }
+                            conn.Close();
+
+                            return listProduct;
+                        }
+                        conn.Close();
+                        return null;
+                    }
                 }
+                catch (Exception e1)
+                {
+                    MessageBox.Show("Failed", "app");
+                    Debug.WriteLine("Error: " + e1.Message);
+                }
+                return null;
+                
             }
         }
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-            SqlConnection conn = DBUtils.GetDBConnection();
-            /*            try
-                        {
-                            conn.Open();
-                            _products = ProductDAO.GetAllProducts(conn);
-                        }
-                        catch (Exception e1)
-                        {
-                            MessageBox.Show("Failed", "app");
-                            Debug.WriteLine("Error: " + e1.Message);
-                        }*/
-            conn.Open();
-            _products = ProductDAO.GetAllProducts(conn);
-            conn.Close();
 
-
-
+            _typeFood = 0;
             _orders = new BindingList<Order>();
             OrderedList.ItemsSource = _orders;
 
+            _products = ProductDAO.GetAllProducts(_typeFood);
             MenuList.ItemsSource = _products;
             MenuList.MouseLeftButtonUp += ChooseProduct;
         }
@@ -201,6 +212,46 @@ namespace QuanLiCantin
 
         private void StackPanel_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
+
+        }
+   
+        private void All_select(object sender, RoutedEventArgs e)
+        {
+            _typeFood = 0;
+            if (_products != null)
+            {
+                _products.Clear();
+                _products = ProductDAO.GetAllProducts(_typeFood);
+                MenuList.ItemsSource = _products;
+            }
+
+        }
+
+        private void Bf_select(object sender, RoutedEventArgs e)
+        {
+            _typeFood = 1;
+            if (_products != null)
+                _products.Clear();
+            _products = ProductDAO.GetAllProducts(_typeFood);
+            MenuList.ItemsSource = _products;
+        }
+
+        private void Dr_select(object sender, RoutedEventArgs e)
+        {
+            _typeFood = 3;
+            if (_products != null)
+                _products.Clear();
+            _products = ProductDAO.GetAllProducts(_typeFood);
+            MenuList.ItemsSource = _products;
+        }
+
+        private void La_select(object sender, RoutedEventArgs e)
+        {
+            _typeFood = 2;
+            if (_products != null)
+                _products.Clear();
+            _products = ProductDAO.GetAllProducts(_typeFood);
+            MenuList.ItemsSource = _products;
 
         }
     }
